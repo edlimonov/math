710fdf3733d44407793c1cefc6872b54846b182d
Nail Zaripov <n.zaripov@vkteam.ru>
2026-05-01 23:56:05 +0300

    Test version of minimax approximation of H_0 on (0, 4]

--- include/boost/math/special_functions/detail/struve_h0.hpp
+++ include/boost/math/special_functions/detail/struve_h0.hpp
@@ -34,20 +34,22 @@ BOOST_MATH_GPU_ENABLED T struve_h0(T x)
 {
 
     BOOST_MATH_STATIC const T P[] = {
-         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, -0.0707355302630645936750594539914)),
-         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 0.00792106043964393838700433537069)),
-         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, -0.000459347160858328152666975152103)),
-         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 0.0000127570704598573950427855592976)),
-         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, -0.000000177610756112727457308380231305)),
-         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 0.00000000100365914526111521466630492207))
+         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, -0.0707355302630645936750591582287)),
+         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 0.00207185634168603648672328873045)),
+         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, -0.0000314267803061135186741922283008)),
+         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 0.000000240524915142590080089672932483)),
+         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, -0.00000000100629922419318636560262041863)),
+         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 2.19727473870844852931259370195e-12)),
+         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, -2.00671658115837495154166406484e-15))
     };
     BOOST_MATH_STATIC const T Q[] = {
          static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 1.0)),
-         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 0.0480186462137823940239219913993)),
-         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 0.00111562640140860204136655251856)),
-         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 0.0000159678117968420435240449310321)),
-         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 0.000000144009832908415329312877296644)),
-         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 6.73639642330268552035075149213e-10))
+         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 0.010709821019495754756436869509)),
+         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 0.0000563519497780850996300711774198)),
+         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 0.000000189131392123180385940303583515)),
+         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 4.34461726487495401100979798332e-10)),
+         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 6.61778054381056332955101039727e-13)),
+         static_cast<T>(BOOST_MATH_BIG_CONSTANT(T, 64, 5.37200932001096714788701957446e-16))
     };
 
     T value;
@@ -59,15 +61,14 @@ BOOST_MATH_GPU_ENABLED T struve_h0(T x)
     // XXX: correct?
     BOOST_MATH_ASSERT(x > 0); // reflection handled elsewhere.
 
-    if (x <= 2)                       // x in (0, 2]
+    if (x <= 4)                       // x in (0, 4]
     {
         T y = x * x;
-        T z = y / 4;
         BOOST_MATH_ASSERT(sizeof(P) == sizeof(Q));
-        T r = evaluate_rational(P, Q, z);
+        T r = evaluate_rational(P, Q, y);
         value = x * (two_div_pi<T>() + y * r);
     }
-    else                                // x in (2, \infty)
+    else                                // x in (4, \infty)
     {
           T y = 2 / x;
           T y2 = y * (1 / pi<T>());
